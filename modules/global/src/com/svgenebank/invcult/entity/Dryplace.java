1233615c5a2d8a6c9565add3cc984b27f6cdaa4d
Florin Tanasă <florin.tanasa@gmail.com>
2022-02-08 12:15:57 +0200

    Creare entitate Dryplace

---
+++ modules/global/src/com/svgenebank/invcult/entity/Dryplace.java
@@ -0,0 +1,28 @@
+package com.svgenebank.invcult.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "INVCULT_DRYPLACE")
+@Entity(name = "invcult_Dryplace")
+@NamePattern("%s|name")
+public class Dryplace extends StandardEntity {
+    private static final long serialVersionUID = -8604966959077584332L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
