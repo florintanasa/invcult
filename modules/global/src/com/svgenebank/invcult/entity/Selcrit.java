445f3ad57c89e79456e8489ca32e29a65cdcc515
Florin Tanasă <florin.tanasa@gmail.com>
2022-02-08 12:17:13 +0200

    Creare entitate Selcrit

---
+++ modules/global/src/com/svgenebank/invcult/entity/Selcrit.java
@@ -0,0 +1,28 @@
+package com.svgenebank.invcult.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "INVCULT_SELCRIT")
+@Entity(name = "invcult_Selcrit")
+@NamePattern("%s|name")
+public class Selcrit extends StandardEntity {
+    private static final long serialVersionUID = -5469914450039509385L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
