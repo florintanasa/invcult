a9188810e232c39ab523aaf99e4cf0832493e705
Florin Tanasă <florin.tanasa@gmail.com>
2022-02-08 12:16:13 +0200

    Creare entitate Motfarmer

---
+++ modules/global/src/com/svgenebank/invcult/entity/Motfarmer.java
@@ -0,0 +1,28 @@
+package com.svgenebank.invcult.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "INVCULT_MOTFARMER")
+@Entity(name = "invcult_Motfarmer")
+@NamePattern("%s|name")
+public class Motfarmer extends StandardEntity {
+    private static final long serialVersionUID = -7041165197888314875L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
