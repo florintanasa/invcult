1bc1b3bafadc0bf90d331930e4929b42e65e6618
Florin Tanasă <florin.tanasa@gmail.com>
2022-02-08 12:17:29 +0200

    Creare entitate Storcont

---
+++ modules/global/src/com/svgenebank/invcult/entity/Storcont.java
@@ -0,0 +1,28 @@
+package com.svgenebank.invcult.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "INVCULT_STORCONT")
+@Entity(name = "invcult_Storcont")
+@NamePattern("%s|name")
+public class Storcont extends StandardEntity {
+    private static final long serialVersionUID = 2893579951084077506L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
