f532f61e78739aa30a3b4dbf947f6b94d0120607
Florin Tanasă <florin.tanasa@gmail.com>
2022-02-08 12:15:41 +0200

    Creare entitate Cultmet

---
+++ modules/global/src/com/svgenebank/invcult/entity/Cultmet.java
@@ -0,0 +1,28 @@
+package com.svgenebank.invcult.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "INVCULT_CULTMET")
+@Entity(name = "invcult_Cultmet")
+@NamePattern("%s|name")
+public class Cultmet extends StandardEntity {
+    private static final long serialVersionUID = -1837607193958552045L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
