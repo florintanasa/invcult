b8bb226dce10d7a5448d7b6b01ee7d7b92048ee3
Florin Tanasă <florin.tanasa@gmail.com>
2022-02-08 12:16:40 +0200

    Creare entitate Oldvar

---
+++ modules/global/src/com/svgenebank/invcult/entity/Oldvar.java
@@ -0,0 +1,28 @@
+package com.svgenebank.invcult.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "INVCULT_OLDVAR")
+@Entity(name = "invcult_Oldvar")
+@NamePattern("%s|name")
+public class Oldvar extends StandardEntity {
+    private static final long serialVersionUID = -8199643432515852472L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
