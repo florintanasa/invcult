2b78c1324c1a9942b6f796f6301c9e4840fd9a83
Florin Tanasă <florin.tanasa@svgenebank.ro>
2022-09-06 15:13:27 +0300

    Added the Map screen and init the map on Map screen and in localitysiruta editor screen using vaadin Leaflet addon lib.

--- modules/web/src/com/svgenebank/invcult/web/screens/map/MapScreen.java
+++ modules/web/src/com/svgenebank/invcult/web/screens/map/MapScreen.java
@@ -1,26 +1,28 @@
-package com.svgenebank.invcult.web.screens.localitysiruta;
+package com.svgenebank.invcult.web.screens.map;
 
 import com.haulmont.cuba.gui.components.VBoxLayout;
-import com.haulmont.cuba.gui.screen.*;
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.Subscribe;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
 import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
-import com.svgenebank.invcult.entity.Localitysiruta;
 import com.vaadin.ui.Layout;
+import com.vaadin.server.Resource;
+import com.vaadin.ui.Component;
 import org.vaadin.addon.leaflet.LMap;
 import org.vaadin.addon.leaflet.LOpenStreetMapLayer;
 import org.vaadin.addon.leaflet.shared.Point;
 
 import javax.inject.Inject;
 
-@UiController("invcult_Localitysiruta.edit")
-@UiDescriptor("localitysiruta-edit.xml")
-@EditedEntityContainer("localitysirutaDc")
-@LoadDataBeforeShow
-public class LocalitysirutaEdit extends StandardEditor<Localitysiruta> {
+@UiController("invcult_MapScreen")
+@UiDescriptor("map-screen.xml")
+public class MapScreen extends Screen {
     private static final double DEFAULT_LATITUDE = 46.009628;
     private  static final double DEFAULT_LONGITUDE = 24.456255;
     private static final double ZOOM_LEVEL =7.0;
 
-    private LMap map = new LMap();
+    private LMap map;
 
     @Inject
     private VBoxLayout mapContainer;
@@ -30,6 +32,7 @@ public class LocalitysirutaEdit extends StandardEditor<Localitysiruta> {
         initMap();
         addMapToContainer();
     }
+
     private void initMap() {
         map = new LMap();
         map.setZoomLevel(ZOOM_LEVEL);
@@ -41,5 +44,4 @@ public class LocalitysirutaEdit extends StandardEditor<Localitysiruta> {
         Layout layout = (Layout) WebComponentsHelper.unwrap(mapContainer);
         layout.addComponent(map);
     }
-
 }
