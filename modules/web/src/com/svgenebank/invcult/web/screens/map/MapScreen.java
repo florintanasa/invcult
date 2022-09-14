160d7d24206db45ee52b3344eaf664717fde8ee8
Florin Tanasă <florin.tanasa@svgenebank.ro>
2022-09-14 12:20:59 +0300

    Created button and methods for this to see localities on the map.

--- modules/web/src/com/svgenebank/invcult/web/screens/map/MapScreen.java
+++ modules/web/src/com/svgenebank/invcult/web/screens/map/MapScreen.java
@@ -1,6 +1,8 @@
 package com.svgenebank.invcult.web.screens.map;
 
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.VBoxLayout;
+import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
 import com.svgenebank.invcult.entity.InvcultView;
@@ -10,6 +12,7 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.Notification;
 import org.vaadin.addon.leaflet.*;
 import org.vaadin.addon.leaflet.shared.Point;
+import org.vaadin.addon.leaflet.shared.PopupState;
 
 import javax.inject.Inject;
 
@@ -20,41 +23,107 @@ import javax.inject.Inject;
 public class MapScreen extends StandardLookup<InvcultView> {
     private static final double DEFAULT_LATITUDE = 46.009628;
     private  static final double DEFAULT_LONGITUDE = 24.456255;
-    private static final double ZOOM_LEVEL =7.0;
+    private static final double ZOOM_SNAP = 0.25;
+    private static final double ZOOM_DELTA = 0.25;
+    private static final double ZOOM_LEVEL =7.5;
 
     private LMap map;
     private LLayerGroup localityMarkers;
-
+    private PopupState popupState;
     @Inject
     private VBoxLayout mapContainer;
+    @Inject
+    private CollectionContainer<InvcultView> invcultViewsDc;
+    @Inject
+    private Button showOnMap;
+
+
 
     @Subscribe
     protected void onInit(InitEvent event) {
+        localityMarkers = new LLayerGroup();
+        popupState = createPopupState();
         initMap();
-        drawLocalityMarkers();
+ //       drawCenterMark();
         addMapToContainer();
 
     }
-
+    /**
+     * Creates the map.
+     * Sets default center of the map and defaults zoom levels.
+     * Adds a layer that uses OSM tiles.
+     */
     private void initMap() {
         map = new LMap();
+        map.setZoomDelta(ZOOM_DELTA);
+        map.setZoomSnap(ZOOM_SNAP);
         map.setZoomLevel(ZOOM_LEVEL);
         map.addLayer(new LOpenStreetMapLayer());
         map.setCenter(new Point(DEFAULT_LATITUDE, DEFAULT_LONGITUDE));
     }
 
+    /**
+     * Creates addMapToContainer method for add Map component in vbox container
+     */
     private void addMapToContainer() {
         Layout layout = (Layout) WebComponentsHelper.unwrap(mapContainer);
         layout.addComponent(map);
     }
 
-    private void drawLocalityMarkers() {
-        Point localityLocation = new Point(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
-        LMarker localityMarker = new LMarker(localityLocation);
-        localityMarker.setPopup("Punctul de cetrare a hărții");
-        localityMarker.openPopup();
+    /**
+     * Creates drawCenterMark this method is used for me on the test
+     */
+    private void drawCenterMark() {
+        Point centerMap = new Point(DEFAULT_LATITUDE,DEFAULT_LONGITUDE);
+        LMarker centerMarker = new LMarker(centerMap);
+        centerMarker.setPopup("Punctul de centrare a hărții");
+        centerMarker.openPopup();
+        centerMarker.setPopupState(popupState);
+        map.addComponent(centerMarker);
+    }
+
+    /**
+     * Creates createPopupState that method set the {@link PopupState} for the markers
+     * @return popupState
+     */
+    private PopupState createPopupState() {
+        PopupState popupState = new PopupState();
+        popupState.autoClose = false;
+        popupState.closeOnClick = false;
+        return popupState;
+    }
+    /**
+     * Creates localityMarkers that contains map layers (markers in our case) to handle them as one in our code.
+     * Refreshes invcultview DataContainer.
+     * For each Salesperson creates the marker that will be added to the personMarkers.
+     * Finally adds personMarkers to the map.
+     */
+
+    public void drawLocalityMarkers() {
+        map.removeComponent(localityMarkers);
         localityMarkers = new LLayerGroup();
-        localityMarkers.addComponent(localityMarker);
+
+        for (InvcultView locality : invcultViewsDc.getItems()) {
+            LMarker localityMarker = createLocalityMarker(locality);
+            localityMarkers.addComponent(localityMarker);
+        }
         map.addComponent(localityMarkers);
+    }
+
+    /**
+     * Creates marker for the passed to the method InvcultView.
+     * Sets popup ?
+     *
+     * @param locality InvcultView passed to the method
+     * @return Localities marker that represents it's location
+     */
+    private LMarker createLocalityMarker(InvcultView locality) {
+        Point localityLocation = new Point(locality.getLatitudine(), locality.getLongitudine());
+        LMarker localityMarker = new LMarker(localityLocation);
+        localityMarker.setPopup(locality.getLocalitateFerma());
+        localityMarker.openPopup();
+        localityMarker.setPopupState(popupState);
+
+        return localityMarker;
     }
 }
